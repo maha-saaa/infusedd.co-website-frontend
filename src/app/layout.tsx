9daab6b538239193e2cac15f1ab46a6205f825a0
Mahsa Badiei <mahsaa.badiee@gmail.com>
2024-05-01 20:39:40 +0330

    Change folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
-import "./styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import Navbar from "./shared/components/navbar/navbar";
+import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {/* <Navbar /> */}
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
